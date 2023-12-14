4119f47531ded59d14c0122b56a072de828a4a85
Svilen Grandzhan <peructep@gmail.com>
2023-12-14 11:13:32 -0700

    Working SelectedProject component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import { useState } from "react";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  const handleSelectProject = (id) => {
+    setProjectsState((previousState) => {
+      return { ...previousState, selectedProjectId: id };
+    });
+  };
 
   const handleCancelAddProject = () => {
     setProjectsState((previousState) => {
@@ -37,7 +44,9 @@ function App() {
     });
   };
 
-  let content;
+  const selectedProject = projectsState.projects.find((project) => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = (
@@ -53,6 +62,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar
+        onSelectProject={handleSelectProject}
         onAddProject={handleAddProjectId}
         projects={projectsState.projects}
       />
